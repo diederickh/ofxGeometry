085afe6241eaa3cb01808dc1e57508ba32d85478
roxlu <diederick@apollomedia.nl>
2011-06-21 23:34:32 +0200

    Add

--- src/ofxGts.h
+++ src/ofxGts.h
@@ -4,6 +4,17 @@
 
 #include "ofxGtsSurface.h"
 
+/*
+  Point - a point in 3D space
+  Vertex - a Point in 3D space that may be used to define a Segment
+  Segment - a line defined by two Vertex end-points
+  Edge - a Segment that may be used to define the edge of a Triangle
+  Triangle - a triangle defined by three Edges
+  Face - a Triangle that may be used to define a face on a Surface
+  Surface - a surface composed of Faces
+*/
+
+
 using  namespace std;
 
 class ofxGts {
